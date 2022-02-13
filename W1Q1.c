bd5b8debe404cf581e3cfc293129e0844e9440d3
ashutoshzuzazu <81466197+ashutoshzuzazu@users.noreply.github.com>
2022-02-13 09:22:40 +0530

    Week 1 assignment
    Completed

---
+++ W1Q1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{printf("Name: Ashutosh Rana ");
+printf("Regn No: 077 ");
+printf("Branch: Civil ");
+printf("Hobbies: Reading,Exploring,Travelling,Cooking,Cricket ");
+return (0);}
